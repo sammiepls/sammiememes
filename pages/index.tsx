0fc0fbd88e41f4498c9519765e270d6fa957613a
Samantha Lau <samantha@coingecko.com>
2021-06-13 22:22:23 +0800

    Setup absolute imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { fetchContent } from "../utils/contentful";
+import styles from "styles/Home.module.css";
+import { fetchContent } from "utils/contentful";
 
 export default function Home({ jokes }) {
   return (
