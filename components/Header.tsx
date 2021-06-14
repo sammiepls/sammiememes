2953f6ff20285fc58dd08810130b32fb4335cb46
Samantha Lau <samantha@coingecko.com>
2021-06-14 22:17:33 +0800

    Style custom yellow shadow for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
 const Header: React.FC = () => {
   return (
     <header className="flex flex-col items-center p-10">
-      <div className="max-w-md px-20 py-2 mb-10 block-shadow">
-        <h1 className="tracking-wide">sammiememes</h1>
+      <div className="max-w-md px-20 py-2 mb-10 block-shadow yellow-shadow">
+        <h1 className="tracking-widest">sammiememes</h1>
       </div>
       <p>A curated library of horrible jokes and my favourite memes</p>
     </header>
