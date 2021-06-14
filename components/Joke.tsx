e475bbd9a978661b71af8a6b5d187daaf8d22431
Samantha Lau <samantha@coingecko.com>
2021-06-14 11:03:30 +0800

    Fix up eslint and prettier plugins

--- components/Joke.tsx
+++ components/Joke.tsx
@@ -1,5 +1,7 @@
-const Joke = ({ content }: string) => {
-  return <div>{content}</div>;
+import JokeProp from 'utils/contentTypes/Jokes';
+
+const Joke: React.FC<JokeProp> = ({ content }) => {
+  return <article className="border-4 border-black">{content}</article>;
 };
 
 export default Joke;
