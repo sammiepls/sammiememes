f0a5ed0bcc1f9567ce16a6608c3b5ab0b6be8dae
Samantha Lau <samantha@coingecko.com>
2021-06-14 22:00:30 +0800

    Edit footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,17 +32,12 @@ const Home: React.FC<Props> = ({ jokes }) => {
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+      <footer className="text-center m-4">
+        Built with ❤️ by{' '}
+        <a rel="noreferrer" target="_blank" href="https://sammiepls.github.io">
+          <span className="font-black">Sammie</span>
         </a>
+        .
       </footer>
     </div>
   );
