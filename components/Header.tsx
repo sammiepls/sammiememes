b16e7c00cb5ad1094b40185ce748ffcbc3e62d53
Samantha Lau <samantha@coingecko.com>
2021-06-14 11:03:50 +0800

    Create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1 className="tracking-wide">sammiememes</h1>
+      <p>A curated library of horrible jokes and my favourite memes</p>
+    </header>
+  );
+};
+
+export default Header;
