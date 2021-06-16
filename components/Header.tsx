b7af0a0c75c7a3ec4f16f04eb2cb2dc41243fbad
Samantha Lau <samantha@coingecko.com>
2021-06-16 20:23:44 +0800

    Add decorative geometry to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,13 @@
+import Triangle from './Triangle';
+import Circle from './Circle';
+
 const Header: React.FC = () => {
   return (
     <header className="flex flex-col items-center p-10">
-      <div className="max-w-md px-20 py-2 mb-10 block-shadow yellow-shadow">
+      <div className="max-w-md px-16 md:px-20 py-2 mb-10 block-shadow yellow-shadow">
         <h1 className="tracking-widest">sammiememes</h1>
+        <Triangle />
+        <Circle />
       </div>
       <p>A curated library of horrible jokes and my favourite memes</p>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       white: '#fff',
       black: '#000',
       yellow: '#FAFED1',
+      coral: '#F68181',
     },
   },
   variants: {
