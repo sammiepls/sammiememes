3343bdb35f0d8a047b1e57a0f1811509c61d5d86
Samantha Lau <samantha@coingecko.com>
2021-06-17 08:56:41 +0800

    Add env to next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const path = require('path');
 
 module.exports = {
+  contentfulSpaceID: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  contentfulAccessToken: process.env.NEXT_PUBLIC_CONTENTFL_ACCESS_TOKEN,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
