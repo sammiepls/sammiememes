ddca40bf7b85bbdc3e23b1c672f398b417a9eb21
Samantha Lau <samantha@coingecko.com>
2021-06-18 16:26:03 +0800

    Add react-markdown to handle markdown formatting

--- components/Joke.tsx
+++ components/Joke.tsx
@@ -1,7 +1,12 @@
 import { JokeProp } from 'utils/contentTypes/Jokes';
+import ReactMarkdown from 'react-markdown';
 
 const Joke: React.FC<JokeProp> = ({ content }) => {
-  return <article className="block-shadow p-10 mb-6">{content}</article>;
+  return (
+    <article className="block-shadow p-10 mb-8">
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </article>
+  );
 };
 
 export default Joke;
