e35e7c62b12094bcbf5055be9423beaf34d143bb
Samantha Lau <samantha@coingecko.com>
2021-06-19 22:11:39 +0800

    Add style for load more button

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Circle from './Circle';
 const Header: React.FC = () => {
   return (
     <header className="flex flex-col items-center p-10">
-      <div className="max-w-md px-16 md:px-20 py-2 mb-10 block-shadow yellow-shadow">
+      <div className="max-w-md px-16 md:px-20 py-2 mb-10 block-shadow yellow-shadow bg-white">
         <h1 className="tracking-widest">sammiememes</h1>
         <Triangle />
         <Circle />
--- components/Joke.tsx
+++ components/Joke.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown';
 
 const Joke: React.FC<JokeProp> = ({ content }) => {
   return (
-    <article className="block-shadow p-10 mb-8">
+    <article className="block-shadow bg-white p-10 mb-8">
       <ReactMarkdown>{content}</ReactMarkdown>
     </article>
   );
