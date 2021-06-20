9b25efd22ef1b664f96f15b06471162956bdb1bb
sammiepls <samantha.lhe@gmail.com>
2021-06-20 06:09:30 +0000

    Deploying to gh-pages from @ sammiepls/sammiememes@7f3520a53d5ebd5d5c42db15cf911df5e3891f87 🚀

---
+++ _next/static/oPF0EwmtAOse7t_qLoxOG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/562-7a908ef2372fcf73d2d5.js","static/chunks/pages/index-b20eb0794cfee4bb0dba.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
